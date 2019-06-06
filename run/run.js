dce94e95373e9374c57d210900b0e3cb79f48346
anrobin <50176492+anrobin719@users.noreply.github.com>
2019-06-07 00:34:59 +0900

    change background, text

--- run/run.js
+++ run/run.js
@@ -7,14 +7,23 @@ $(function() {
         //move left
         case 37:
             hero.animate({left: "-=50px"}, 'fast');
-            $("#move").css({'animation': 'run .7s steps(1) infinite'});
             break;
         
         //move right
         case 39:
             hero.animate({left: "+=50px"}, 'fast');
             break;
+        
+        //jump
+        case 38:
+            hero.css({'animation': 'jump .7s steps(5) infinite'});
+            break;
         };
+    }).on("keydown", function(){
+        $("#move").css({'animation': 'run .7s steps(6) infinite'});
+    }).on("keyup", function(){
+        $("#move").css({'animation': 'stay 1s steps(5) infinite'});
     });
+
 
 });
